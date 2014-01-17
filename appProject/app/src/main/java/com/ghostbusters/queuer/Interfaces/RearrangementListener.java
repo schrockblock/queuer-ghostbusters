1060b8545b9e29e0895202a936450b5f36d7bea5
Patrick Johnson <Patrick@Patricks-MacBook-Pro-2.local>
2014-01-16 20:26:41 -0500

    added EnhancedListView

---
+++ appProject/app/src/main/java/com/ghostbusters/queuer/Interfaces/RearrangementListener.java
@@ -0,0 +1,11 @@
+package de.timroes.android.listview;
+package com.ghostbusters.queuer.Interfaces;
+
+/**
+ * Created by eschrock on 12/26/13.
+ */
+public interface RearrangementListener {
+    public void onStartedRearranging();
+    public void swapElements(int indexOne, int indexTwo);
+    public void onFinishedRearranging();
+}
